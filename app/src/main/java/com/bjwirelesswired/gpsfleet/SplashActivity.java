7577bd5fda3be61f12230fea7d085d9c8554cf53
krishnakumar <softeng.krishna@gmail.com>
2016-04-18 17:33:23 +0530

    dwdwdw

---
+++ app/src/main/java/com/bjwirelesswired/gpsfleet/SplashActivity.java
@@ -0,0 +1,68 @@
+package com.bjwirelesswired.gpsfleet;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+
+public class SplashActivity extends AwesomeSplash {
+
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.colorPrimary); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.mipmap.ic_launcher); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(1000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+
+
+        //Customize Title
+        configSplash.setTitleSplash(getResources().getString(R.string.app_name));
+        configSplash.setTitleTextColor(R.color.colorWhite);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(1000);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+        configSplash.setTitleFont("fonts/iosfont.otf"); //provide string to your font located in assets/fonts/
+/*
+
+        boolean mboolean = false;
+        SharedPreferences settings = getSharedPreferences("PREFS_NAME", 0);
+        mboolean = settings.getBoolean("FIRST_RUN", false);
+        if (!mboolean) {
+
+            // do the thing for the first time
+            settings = getSharedPreferences("PREFS_NAME", 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("FIRST_RUN", true);
+            editor.commit();
+
+            setCurrency();
+
+        }
+*/
+
+
+    }
+
+    @Override
+    public void animationsFinished() {
+
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+
+
+    }
+}
