fa23c62f0ea026610143886dfa91935c1f6c3750
krishnakumar <softeng.krishna@gmail.com>
2016-04-19 10:47:45 +0530

    dwdwdw

--- app/src/main/java/com/bjwirelesswired/gpsfleet/SplashActivity.java
+++ app/src/main/java/com/bjwirelesswired/gpsfleet/SplashActivity.java
@@ -2,68 +2,34 @@ package com.bjwirelesswired.gpsfleet;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-import com.daimajia.androidanimations.library.Techniques;
-import com.viksaa.sssplash.lib.activity.AwesomeSplash;
-import com.viksaa.sssplash.lib.cnst.Flags;
-import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+public class SplashActivity extends AppCompatActivity {
 
 
-public class SplashActivity extends AwesomeSplash {
 
     @Override
-    public void initSplash(ConfigSplash configSplash) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
         getSupportActionBar().hide();
-        //Customize Circular Reveal
-        configSplash.setBackgroundColor(R.color.colorPrimary); //any color you want form colors.xml
-        configSplash.setAnimCircularRevealDuration(2000); //int ms
-        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
-        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
 
-        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
 
-        //Customize Logo
-        configSplash.setLogoSplash(R.drawable.logosmall); //or any other drawable
-        configSplash.setAnimLogoSplashDuration(1000); //int ms
-        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
-
-
-
-        //Customize Title
-        configSplash.setTitleSplash(getResources().getString(R.string.app_name));
-        configSplash.setTitleTextColor(R.color.colorWhite);
-        configSplash.setTitleTextSize(30f); //float value
-        configSplash.setAnimTitleDuration(1000);
-        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
-        configSplash.setTitleFont("fonts/iosfont.otf"); //provide string to your font located in assets/fonts/
-/*
-
-        boolean mboolean = false;
-        SharedPreferences settings = getSharedPreferences("PREFS_NAME", 0);
-        mboolean = settings.getBoolean("FIRST_RUN", false);
-        if (!mboolean) {
-
-            // do the thing for the first time
-            settings = getSharedPreferences("PREFS_NAME", 0);
-            SharedPreferences.Editor editor = settings.edit();
-            editor.putBoolean("FIRST_RUN", true);
-            editor.commit();
-
-            setCurrency();
-
-        }
-*/
+        },1500);
 
 
     }
 
-    @Override
-    public void animationsFinished() {
 
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
-            finish();
-
-
-    }
 }
